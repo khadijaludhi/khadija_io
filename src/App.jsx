cbc59fa7595ec485ca63402b0fcd0b7006ccb417
khadijaludhi <khadijaludhi@gmail.com>
2023-09-08 21:22:48 +0100

    imported StarsCanvas into the intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Intro from "./components/Intro";
+import StarsCanvas from "./components/canvas/Stars";
 
 function App() {
 
  return (
-     <div className='App'>
-       <div id='content'>
-       <Intro></Intro>
-
-       </div>
-     </div>
+      <div className='App'>
+        <div id='content'>
+          <div className='z-0'>
+            <Intro></Intro>
+            
+            <StarsCanvas></StarsCanvas>
+          </div>
+        </div>
+      </div>
  )
 }
 
