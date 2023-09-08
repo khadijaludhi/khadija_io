99d44eed48874dc9d8aa83aae80d1446d7682e18
khadijaludhi <khadijaludhi@gmail.com>
2023-09-08 22:24:55 +0100

    typewriter starts when user scrolls to section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import '../styles/About.css';
 import Typewriter from 'typewriter-effect';
 
 const AboutMe = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1,
+      }
+    );
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div id="about">
+    <div id="about" ref={sectionRef}>
       <div className="about-container">
         <div className="section-header">
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter
-              .typeString('About Me')
-              .start();
-            }}
-          />
+          {isVisible && (
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString('About Me')
+                  .start();
+              }}
+            />
+          )}
         </div>
         <div className="about-content">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p>Lorem ipsum...</p>
+          <p>Lorem ipsum...</p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default AboutMe;
