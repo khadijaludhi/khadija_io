802ee18dde803e2624c1e2372807dff21f93306b
khadijaludhi <khadijaludhi@gmail.com>
2023-09-08 20:02:53 +0100

    downloaded lottie animation and added it to intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,33 +1,29 @@
-import React from "react";
 import "../styles/Intro.css";
+import Lottie from 'lottie-react';
+import Robot from "../assets/robotsayshi.json";
 
 const Intro = () => {
-   const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
-
-   React.useEffect(() => {
-       const handleResize = () => setWindowWidth(window.innerWidth);
-
-       window.addEventListener('resize', handleResize);
-
-       return () => {
-           window.removeEventListener('resize', handleResize);
-       };
-   }, []);
-
-   return (
-       <div id="intro">
-           <div className="intro-container">
-               <div className="dialogue">hey there,</div>
-               <span className="dialogue">
-                   <span className="intro-name">khadija</span>
-                   {" here :)"}
-               </span>
-           </div>
-       </div>
-   );
+    return (
+        <div id="intro">
+            <div className="intro-container">
+                <div className="lottie-container">
+                    <Lottie 
+                        animationData={Robot}
+                        loop={true}
+                        autoPlay={true}
+                        rendererSettings={{
+                            preserveAspectRatio: 'xMidYMid slice'
+                        }}
+                    />
+                </div>
+                <div className="dialogue">
+                    <span className="intro-greeting">hey there,</span>
+                    <br />
+                    <span className="intro-name">Khadija</span> <span className="intro-greeting">here :)</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
-
 export default Intro;
-
-
