ef34e5d5aa59f836cb0418ec84d8587231ebd21a
khadijaludhi <khadijaludhi@gmail.com>
2023-09-09 14:19:41 +0100

    updated projects section, typewriter works and i added some filler text i can change later

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,61 @@
-import "../styles/Projects.css";
+import React, { useEffect, useState, useRef } from 'react';
+import '../styles/Projects.css';
 import Typewriter from 'typewriter-effect';
+import Fade from 'react-reveal/Fade';
 
-const Intro = () => {
-    return (
-        <div id="project">
-            <div className="project-container">
+const Projects = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef(null);
 
-                <div className="dialogue">
-                    <Typewriter
-                        options={{
-                            delay: 85,  // Setting the speed to 90 milliseconds per character
-                        }}
-                        onInit={(typewriter) => {
-                            typewriter
-                            .typeString('<span class="intro-greeting">hey there,</span>')
-                            .typeString('<br />')  // This adds a new line
-                            .typeString('<span class="intro-name">Khadija</span> <span class="intro-greeting">here :)</span>')
-                            .start();
-                        }}
-                    />
-                </div>
-            </div>
-        </div>
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.25,
+      }
     );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div id="projects" ref={sectionRef}>
+      <div className="projects-container">
+        <div className="section-header">
+          {isVisible && (
+            <Typewriter
+              options={{
+                delay: 50,  // Speed in milliseconds
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString('Projects')
+                  .start();
+              }}
+            />
+          )}
+        </div>
+        <Fade bottom when={isVisible} delay={500} distance="75px" duration={1200}>
+          <div className="projects-content">
+            <p>Project 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Project 2: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+          </div>
+        </Fade>
+      </div>
+    </div>
+  );
 };
 
-export default Intro;
+export default Projects;
