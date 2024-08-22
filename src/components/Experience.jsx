094cf40a2c8afa25f1b2a59cebe41b4948829f5b
khadijaludhi <khadijaludhi@gmail.com>
2024-08-22 22:15:07 +0100

    Experience section: add in the work experience next

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,8 +4,6 @@ import { useState, useEffect, useRef } from 'react';
 import '../styles/Experience.css';
 import Typewriter from 'typewriter-effect';
 import Fade from 'react-reveal/Fade';
-import headshot from '../assets/headshot.jpeg'; // Import the image
-
 
 const Experience = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -82,18 +80,17 @@ const Experience = () => {
         <Fade bottom when={isVisible || hasBeenVisible} delay={500} distance="75px" duration={3000}>  {/* <- Changed this condition */}
           <div className="experience-content">
             <div>
-                <p>blank <span className="highlighted-text">blank</span> blank <span className="highlighted-text">blank</span> blank.</p>
-                <p>blank <span className="highlighted-text">blank</span>. blank.</p>
-                <p>blank.</p>
+                <p>Blank Blank <span className="highlighted-text">Blank Blank </span>Blank Blank <span className="highlighted-text">Blank Blank </span>Blank Blank.</p>
+                <p>Blank <span className="highlighted-text">Blank </span> Blank.</p>
             </div>
             
-            </div>
+          </div>
 
         </Fade>
-        </div>
+      </div>
 
     </div>
-    );
+  );
 };
 
 export default Experience;
